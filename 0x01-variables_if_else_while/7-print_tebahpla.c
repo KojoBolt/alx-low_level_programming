9aa741252539e81b75bb57674a24ad3140489122
KojoBolt <kojobolt@gmail.com>
2022-10-17 23:08:48 +0000

    reverse letter

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- prints the lowercase alphabet in reverse
+ *
+ * Return: 0 when sucessfull
+ */
+
+int main(void)
+
+{
+	char d;
+
+	for (d = 'z'; d >= 'a'; d--)
+	{
+	putchar(d);
+	}
+	putchar('\n');
+}
