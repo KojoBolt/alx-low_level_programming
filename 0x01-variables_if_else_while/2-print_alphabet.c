f5a19b11e10d337d181fe5fbfbf4aeeec71003df
KojoBolt <kojobolt@gmail.com>
2022-10-17 21:25:46 +0000

    print out lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- Execute a char character
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		putchar(i);
+	}
+		putchar ('\n');
+
+	return (0);
+
+}
