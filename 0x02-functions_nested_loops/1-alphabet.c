db283d667cedb37f9d4e2c1eb1dde5298ba9239a
KojoBolt <kojobolt@gmail.com>
2022-10-19 23:01:29 +0000

    lowercase numebr

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet- in lowercase, followed by a new line.
+ */
+
+void print_alphabet(void)
+
+{
+	char d;
+		;
+	for (d = 'a'; d < = 'z'; d++)
+	_putchar(d);
+
+	_putchar('\n');
+}
