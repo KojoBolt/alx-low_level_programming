4139e56923a07a8e9383dbcef428b7d9167eee9a
KojoBolt <kojobolt@gmail.com>
2022-10-20 18:04:37 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add- add two int
+ * @a: the first num
+ * @b: the second num
+ * Return: the addition of both answers
+ */
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
