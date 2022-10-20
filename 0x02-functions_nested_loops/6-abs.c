c241bde83b7d0c61be24cb396ec6d43a9c54490b
KojoBolt <kojobolt@gmail.com>
2022-10-20 15:24:30 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs- compute the value of an integer
+ *
+ * @n: the int the questions
+ *
+ * Return: the abs of the int.
+ */
+
+int _abs(int n)
+
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
