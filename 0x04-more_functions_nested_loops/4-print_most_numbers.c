f8f19804b9549aeac4cfba015b0e16f0c87f48f4
KojoBolt <kojobolt@gmail.com>
2022-10-22 12:35:20 +0000

    num printing

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,12 @@
 #include "main.h"
 
 /**
- * print_most_numbers- print most num expect 2 and 4
- *
- * Return: void
- */
-
+*print_most_numbers - prints
+*Return: void
+*/
 
 void print_most_numbers(void)
 {
-
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
