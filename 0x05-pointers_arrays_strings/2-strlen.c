823020fcfa6fd4241f95e742639858bd282dea28
KojoBolt <kojobolt@gmail.com>
2022-10-24 22:56:04 +0000

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  _strlen- returns the length of a string
+ *  @s: string
+ *  Return: lenght
+ */
+
+int _strlen(char *s)
+{
+	int leg = 0;
+
+	while (*s != "\10")
+	{
+		len++;
+		s++;
+	}
+	return (lenght);
+}
